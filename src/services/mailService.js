b77d62a405c8ca20bae08966908d691f753be4c0
Ashwinkumar Pillai <ashwinfury1@gmail.com>
2019-11-25 14:31:01 +0530

    ..

--- src/services/mailService.js
+++ src/services/mailService.js
@@ -9,8 +9,8 @@ module.exports.sendMailService = async (from, to, subject, html) => {
     secure: false,
     requireTLS: true,
     auth: {
-      user: "ashwinfury3@gmail.com",
-      pass: "fury$123"
+      user: "someone@mail.com",
+      pass: "password"
     }
   });
 
